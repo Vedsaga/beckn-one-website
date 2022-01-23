9023f8e69a2622edf5bd8e37ffce9ddbefe681cf
sigmanificient <edhyjox@gmail.com>
2022-01-23 20:46:58 +0100

    :lipstick: Basic design

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,22 @@
 </template>
 
 <style lang="scss">
+:root {
+	--text-color: #333;
+	--bg-color: #fff;
+
+}
+
+body {
+	margin: 0;
+	font-family: 'Montserrat', sans-serif;
+	background-color: var(--bg-color);
+	color: var(--text-color);
+	font-size: 13px;
+}
+
+a {
+	text-decoration: none;
+	color: inherit;
+}
 </style>
