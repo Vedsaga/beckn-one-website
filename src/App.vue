a8837ddf78637926d4f268a2c999754c86c5b181
sigmanificient <edhyjox@gmail.com>
2022-01-23 20:59:00 +0100

    :art: ESLint

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <template>
-	<router-view />
+  <router-view />
 </template>
 
 <style lang="scss">
 :root {
-	--text-color: #333;
-	--bg-color: #fff;
-
+  --text-color: #333;
+  --bg-color: #fff;
 }
 
 body {
-	margin: 0;
-	font-family: 'Montserrat', sans-serif;
-	background-color: var(--bg-color);
-	color: var(--text-color);
-	font-size: 13px;
+  margin: 0;
+  font-family: "Montserrat", sans-serif;
+  background-color: var(--bg-color);
+  color: var(--text-color);
+  font-size: 13px;
 }
 
 a {
-	text-decoration: none;
-	color: inherit;
+  text-decoration: none;
+  color: inherit;
 }
 </style>
