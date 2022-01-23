34214e1c18a9202a10b03edc7162058142f8ac01
Harshit Kumar <harshit7kr@gmail.com>
2022-01-24 02:22:49 +0530

    :sparkles: First HTML

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,38 @@
 <template>
-  <p>Testing</p>
+	<header class="header">
+		<div class="header-left">
+			<div class="logo">
+				becknONE
+			</div>
+			<nav class="header-nav">
+				<ul>
+					<li><a href="#">
+						Getting Started
+					</a></li>
+					<li><a href="#">
+						Resources
+						<ul class="span-list">
+							<li><a href="#">
+								API Reference
+							</a></li>
+							<li><a href="#">
+								Privacy Policy
+							</a></li>
+							<li><a href="#">
+								Terms Conditions
+							</a></li>
+						</ul>
+					</a></li>
+				</ul>
+			</nav>
+		</div>
+		<div class="header-right">
+			continue login
+		</div>
+	</header>
 </template>
-
 <script>
 export default {
-  name: "Home",
+	name: "Home"
 };
 </script>
