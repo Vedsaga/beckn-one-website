06ed44c74e4431bd5a65b5e411b54299e79e74c9
sigmanificient <edhyjox@gmail.com>
2022-01-24 14:01:21 +0100

    :bug: Fixing Login API request (post)

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,35 +1,30 @@
 <template>
-	<HeaderLogin/>
+	<HeaderLogin />
 	<div class="login">
 		<div class="login-grid">
 			<div class="welcome-message">
-				<p>
-					Log in
-				</p>
-				<h1>
-					Welcome Back!
-				</h1>
+				<p>Log in</p>
+				<h1>Welcome Back!</h1>
 			</div>
 			<div class="credentials">
 				<div class="input-field">
-					<input type="text" v-model="username" placeholder="Username">
+					<input type="text" v-model="username" placeholder="Username" />
 				</div>
 				<div class="input-field">
-					<input type="password" v-model="password" placeholder="Password">
+					<input type="password" v-model="password" placeholder="Password" />
 				</div>
 			</div>
 			<div class="login-button" @submit.prevent="login">
-				<button @click="login">
-					Login
-				</button>
+				<button @click="login">Login</button>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 import HeaderLogin from "@/components/headers/HeaderLogin.vue";
+
 export default {
 	name: "LoginPage",
 	components: {
@@ -43,25 +38,18 @@ export default {
 	},
 	methods: {
 		async login() {
-			//encode the username and password to json request
-			let postData = {
-				username: this.username,
-				password: this.password,
-			};
-			// make the request to https://beckn-one.succinct.in/login
-			let response = await axios.post("https://beckn-one.succinct.in/login", postData);
-			// if the response is successful
-			if (response.status === 200) {
-				// store the response in the local storage
-				localStorage.setItem("user", JSON.stringify(response.data));
-				// redirect to the home page
-				console.log(response.data);
-			}
-			else {
-				// if the response is not successful
-				// show the error message
-				console.log(response.data);
-			}
+			await axios
+				.post("https://beckn-one.succinct.in/login", {
+					User: {
+						Name: this.username,
+						Password: this.password,
+					},
+				})
+				.then((res) => {
+					if (res.status === 200) {
+						localStorage.setItem("user", JSON.stringify(res.data));
+					}
+				});
 		},
 	},
 };
